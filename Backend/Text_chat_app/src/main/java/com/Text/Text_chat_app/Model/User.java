ce42ae91230f91f9ac1c9884380255acfa97ce53
Rajathk6 <rajathkumar120@gmail.com>
2025-03-16 14:35:43 +0530

    created model class and postgres database

---
+++ Backend/Text_chat_app/src/main/java/com/Text/Text_chat_app/Model/User.java
@@ -0,0 +1,29 @@
+package com.Text.Text_chat_app.Model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "user")
+
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(unique = true, nullable = false)
+    private String password;
+}
---
+++ Backend/Text_chat_app/src/main/java/com/Text/Text_chat_app/Service/UserService.java
@@ -0,0 +1,20 @@
+package com.Text.Text_chat_app.Service;
+
+import org.springframework.stereotype.Service;
+
+import com.Text.Text_chat_app.Model.User;
+
+@Service
+public class UserService {
+
+    public void authenticateUser(User user) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'authenticateUser'");
+    }
+
+    public void registerUser(User user) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'registerUser'");
+    }
+
+}
