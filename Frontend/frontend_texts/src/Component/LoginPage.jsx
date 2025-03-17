a6c67c14221f7799869b223c2dad4f5199c62c54
Rajathk6 <rajathkumar120@gmail.com>
2025-03-17 23:31:39 +0530

    better design and user acknowledgement

--- Frontend/frontend_texts/src/Component/LoginPage.jsx
+++ Frontend/frontend_texts/src/Component/LoginPage.jsx
@@ -2,31 +2,39 @@ import { useState } from "react";
 import { TbBubbleText } from "react-icons/tb";
 import ApiMapping from "../Config/ApiMapping";
 import toast, { Toaster } from 'react-hot-toast';
-import { useNavigate } from "react-router-dom"; // Import useNavigate
+import { useNavigate } from "react-router-dom";
 
 function LoginPage() {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const navigate = useNavigate(); 
 
+    
+
     const handleButtonClick = async (e) => {
         e.preventDefault();
+
+        const response = ApiMapping.post("/api/auth/login", { username, password }); 
+        toast.promise(
+            response,
+            {
+                loading: 'Logging in...', 
+                success: 'Logged in successfully!',
+                error: 'Could not sign in. Please check your credentials.'
+            }
+        )   
         
         try {
-            const response = await ApiMapping.post("/api/auth/login", { username, password });
-
-            console.log("Login Successful:", response.data);
-            toast.success("Logged in successfully!");
-            
-            // Navigate to dashboard after successful login
+            const loginpromise = await response
+            console.log("Login Successful:", loginpromise.data);
             navigate("/dashboard");
 
         } catch (error) {
             console.log(username)
             console.log(password)
-            console.error("Login Error:", error.response?.data || error.message);
-            toast.error("Failed to log in");
+            console.error("Login Error:", error.loginpromise?.data || error.message);
         }
+        
     };
 
     return (
@@ -44,7 +52,7 @@ function LoginPage() {
             <div className="auth-form-container">
                 <h2>Login or create your account with <br />Text</h2>
 
-                <form onSubmit={handleButtonClick}>
+                <form className="form-attributes" onSubmit={handleButtonClick}>
                     <input onChange={(e) => setUsername(e.target.value)} type="text" name="username" placeholder="Enter UserName " required /><br />
                     <input onChange={(e) => setPassword(e.target.value)} type="password" name="password" placeholder="Enter Password " required /><br />
                     <button type="submit" className="loginbutton">Log in</button>
