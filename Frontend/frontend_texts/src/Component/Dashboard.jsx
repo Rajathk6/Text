bc8dd8f31c2363729657c4733c4bb3766152a8be
Rajathk6 <rajathkumar120@gmail.com>
2025-06-03 14:29:49 +0530

    ready for production

--- Frontend/frontend_texts/src/Component/Dashboard.jsx
+++ Frontend/frontend_texts/src/Component/Dashboard.jsx
@@ -140,7 +140,7 @@ function Dashboard() {
             return;
         }
         const client = new Client({
-            webSocketFactory: () => new SockJS("http://localhost:8080/ws-endpoint"),
+            webSocketFactory: () => new SockJS(apiUrl,"/ws-endpoint"),
             connectHeaders: {
                 Authorization: `Bearer ${localStorage.getItem("token")}`,
                 login: username,
